47eb4446e8ef97373e4c1bb45f635a3f81e08908
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2024-07-30 23:49:11 +0530

    creating teachers route

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,32 @@
-"use client"
+"use client";
 
-import { UserButton } from "@clerk/nextjs"
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
 
 export const NavbarRoutes = () => {
-    return (
-        <div className="flex gap-x-2 ml-auto">
-            <UserButton />
-        </div>
-    )
-}
+  const pathname = usePathname();
+
+  const isTeacherPages = pathname?.startsWith("/teacher");
+  const isPlayerPages = pathname?.includes("/courses");
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPages || isPlayerPages ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton />
+    </div>
+  );
+};
