b46be2690e8782c9052f8b843b8542005af4c9ca
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2024-08-15 23:07:37 +0530

    added category price and attachment

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_compoment/price-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Course } from "@prisma/client";
+import { formatPrice } from "@/lib/format";
+
+interface PriceFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  price: z.coerce.number(),
+});
+
+export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editToggle = () => setIsEditing((prev) => !prev);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      price: initialData?.price || undefined,
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated");
+      editToggle();
+      router.refresh();
+    } catch {
+      toast.error("something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course price
+        <Button variant="ghost" onClick={editToggle}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit price
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            "text-sm mt-2",
+            !initialData.price && "text-slate-500 italic"
+          )}
+        >
+          {initialData.price ? formatPrice(initialData.price) : "No price"}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      step="0.01"
+                      disabled={isSubmitting}
+                      placeholder="set a price for your course"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={!isValid || isSubmitting}>
+              Submit
+            </Button>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,61 +1,112 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_compoment/title-form";
 import { DescriptionForm } from "./_compoment/description-form";
 import { ImageForm } from "./_compoment/image-form";
+import { CategoryForm } from "./_compoment/category-form";
+import { PriceForm } from "./_compoment/price-form";
+import { AttachmentForm } from "./_compoment/attachment-form";
 
-const CourseIdPage = async({ params }: { params: { courseId: string }}) => {
-    const { userId } = auth();
-    if(!userId) {
-        return redirect("/");
-    }
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
 
-    const course = await db.course.findUnique({
-        where: {
-            id: params.courseId
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+        attachments: {
+            orderBy: {
+                createdAt: "desc"
+            }
         }
-    })
-
-    if(!course) {
-        return redirect("/");
     }
+  });
 
-    const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId];
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
-    const totalFields = requiredFields.length;
-    const completedFields = requiredFields.filter(Boolean).length;
+  if (!course) {
+    return redirect("/");
+  }
 
-    const completionText = `(${completedFields}/${totalFields})`;
-    return ( 
-        <div className="p-6">
-            <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-y-2">
-                    <h1 className="text-2xl font-medium">
-                        Course Setup
-                    </h1>
-                    <span className="text-sm text-slate-700">
-                        Complete all the fields {completionText}
-                    </span>
-                </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={LayoutDashboard} />
-                        <h2 className="text-xl">
-                            Customize your course
-                        </h2>
-                    </div>
-                    <TitleForm initialData={course} courseId={course.id} />
-                    <DescriptionForm initialData={course} courseId={course.id} />
-                    <ImageForm initialData={course} courseId={course.id} />
-                </div>
-            </div>
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+  return (
+    <div className="p-6">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all the fields {completionText}
+          </span>
         </div>
-     );
-}
- 
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
+        </div>
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListChecks} />
+              <h2 className="text-xl">Course chapters</h2>
+            </div>
+            <div>
+              TODO: Chapters
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSign} />
+              <h2 className="text-xl">Sell your course</h2>
+            </div>
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Resources & Attachments</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CourseIdPage;
